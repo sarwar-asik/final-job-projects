4c5ef677f22421ef9c3932c315d5d81bf29fe714
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-01-11 11:21:25 +0600

    added

--- src/DashBoard/NoData.jsx
+++ src/DashBoard/NoData.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const NoData = ({ children }) => {
     return (
         <div className=' mx-auto h-96  flex items-center justify-center rounded-xl bg-base-200'>
-            <div className="text-6xl text-center">{children}</div>
+            <div className="text-5xl text-center ">{children}</div>
         </div>
     );
 };
