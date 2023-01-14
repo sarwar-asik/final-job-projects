aa0a3173474855e6aeed4d2fff6a200250329b8a
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-01-14 12:11:44 +0600

    added home page design

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -6,23 +6,37 @@ import Contact from './Contact/Contact';
 import HiringCompany from './HiringCompany';
 import JobCategory from './JobCategory';
 import Login from './Login/Login';
+import Other from './Other';
 import PricingPlan from './PricingPlan/PricingPlan';
 
 const Home = () => {
     return (
         <div>
             {/* this is banner */}
-            <div className=" min-h-screen bg-no-repeat bg-cover" style={{ backgroundImage: `url(${img2})` }}>
-                <div className="flex md:justify-end md:mr-40 md:pt-10 justify-center">
-                    <div>
-                        <h1 className="text-5xl text-black font-bold">Find Your Job</h1>
-                        <p className="py-6 text-black w-[350px]">Find full or part-time jobs in England, Scotland and Wales.
-                            Use the ‘Find a job’ service to search and apply for jobs.
-                            This service has replaced Universal Jobmatch.</p>
-                        <Link to={'/contact'}> <PrimaryButton>Contact US</PrimaryButton></Link>
-                    </div>
+            <div
+            style={{
+                backgroundColor: '#16a085',
+              }}
+            className="hero p-16 mt-5 rounded-xl text-white">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <img src={img2} className=" rounded-lg lg:w-1/2 shadow-2xl" alt='' />
+                <div>
+                    <h1 className="text-5xl font-bold">VlaunChu Jobs !</h1>
+                    <p className="py-6">We are looking for mission-oriented candidates who are passionate about global health. Looking for skilled professionals to help advance our life-saving mission. Trusted information. Fact-based content. Highlights: Donation Option Available, Newsletter Available.</p>
+            <div className="form-control">
+             <div className="input-group">
+             <input type="text" placeholder="Job title,keywords..." className="input input-bordered" />
+                <button className="btn btn-square">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                </button>
+              </div>
+               </div>
                 </div>
             </div>
+        </div>
+        
+        <Other></Other>
+
             {/* this is job category */}
             <JobCategory />
             {/* this is company category */}
